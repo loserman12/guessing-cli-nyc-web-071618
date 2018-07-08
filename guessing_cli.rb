20180d999c530bca7ce689f4c241b1c18749183d
Your Name <you@example.com>
2018-07-08 18:13:19 -0400

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,22 @@
 # Code your solution here!
+def run_guessing_game
+  puts "Guess a number between 1 and 6."
+  input = gets.chomp
+  until input.downcase == "exit"
+    generate = rand(6)
+    if input.to_i == generate
+      puts "You guessed the correct number!"
+      puts "Guess a number between 1 and 6."
+      input = gets.chomp
+    elsif input.to_i != generate
+      puts "The computer guessed #{generate}."
+      puts "Guess a number between 1 and 6."
+      input = gets.chomp
+    end
+  end
+  exit
+end
+
+def exit
+  puts "Goodbye!"
+end
